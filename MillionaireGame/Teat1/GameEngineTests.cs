b154d8b6a02c6d6ee267904e725c0225da0b8a13
HazemAlkhani <hazem007@msn.com>
2024-02-18 06:13:05 +0100

    drone

--- MillionaireGame/Teat1/GameEngineTests.cs
+++ MillionaireGame/Teat1/GameEngineTests.cs
@@ -9,12 +9,12 @@ namespace MillionaireGame.Test1
         public void GetNextQuestion_ShouldReturnNullIfNoMoreQuestionsAvailable()
         {
             // Arrange
-            var competitionQuestions = new List<Question>
+            var competitionQuestions = new List<Question?>
             {
                 new Question("Question 1", new List<string> { "Choice 1", "Choice 2", "Choice 3", "Choice 4" }, 0, 100),
                 new Question("Question 2", new List<string> { "Choice A", "Choice B", "Choice C", "Choice D" }, 2, 200),
             };
-            var helpQuestions = new List<Question>();
+            var helpQuestions = new List<Question?>();
             var gameEngine = new GameEngine(competitionQuestions, helpQuestions);
 
             // Act
--- MillionaireGame/obj/Debug/net8.0/MillionaireGame.AssemblyInfo.cs
+++ MillionaireGame/obj/Debug/net8.0/MillionaireGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MillionaireGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec54939c73dd34de80d222ae6164b2d69cb48748")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd1391a6667a2f137a4313fee1e9a5b980975bc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MillionaireGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MillionaireGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
